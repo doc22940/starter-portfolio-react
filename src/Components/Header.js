733c025c15f314dca244b73db69c06c6194b5f8c
Arif Mohammed P.F <arifmohammedpf77@gmail.com>
2020-08-05 19:52:19 +0530

    Header component

---
+++ src/Components/Header.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar,Nav} from 'react-bootstrap';
+import './Style.css';
+
+
+function Header(){
+
+    return(
+
+        <section className="headerClass">
+            <div className="container-fluid">
+                <div className="nextStyle">
+                    <div className="navClass">
+                    <Navbar bg="transparent" expand="md">
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="ml-auto">
+                                <Nav.Link href="#home" style={{color:"white"}}>Home</Nav.Link>
+                                <Nav.Link href="#link" style={{color:"white"}}>About</Nav.Link>
+                                <Nav.Link href="#link" style={{color:"white"}}>Projects</Nav.Link>
+                                <Nav.Link href="#link" style={{color:"white"}}>Contact</Nav.Link>                                
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Navbar>
+                    </div>
+                    
+
+
+                    <div className="intro">
+                        <img />
+                        <h1 style={{fontSize:'3.5rem'}}>Hello, I'm Arif Mohammed</h1>
+                        <h4>Developer</h4>
+                    </div>
+                    
+
+
+                </div>            
+            </div>
+        </section>
+
+    );
+
+}
+export default Header;
