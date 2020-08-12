ed742af6b45fa58850741ee89f915c808b23d4d2
Arif Mohammed P.F <arifmohammedpf77@gmail.com>
2020-08-12 08:18:23 +0530

    Footer: - for i tag class is replaced with className

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -10,12 +10,12 @@ function Footer(){
 
         <section className="footerSec">
             <footer className="footer">
-                <span className="arrowTop" onClick={scrollTop}><i class='fas fa-angle-double-up'></i></span>
+                <span className="arrowTop" onClick={scrollTop}><i className='fas fa-angle-double-up'></i></span>
                 <div className="row socialFooter">
-                    <a className="icon" rel="noopener noreferrer" target="_blank" href="https://github.com/arifmohammedpf" style={{margin:'0rem'}}><i class='fab fa-github'></i></a>
-                    <a className="icon" rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/arifmohammedpf"><i class='fab fa-linkedin'></i></a>
-                    <a className="icon" rel="noopener noreferrer" target="_blank" href="mailto:arifmohammedofficial@gmail.com"><i class='fab fa-google'></i></a>
-                    <a className="icon" rel="noopener noreferrer" target="_blank" href="https://www.instagram.com/arifmuhammed_0"><i class='fab fa-instagram'></i></a> 
+                    <a className="icon" rel="noopener noreferrer" target="_blank" href="https://github.com/arifmohammedpf" style={{margin:'0rem'}}><i className='fab fa-github'></i></a>
+                    <a className="icon" rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/arifmohammedpf"><i className='fab fa-linkedin'></i></a>
+                    <a className="icon" rel="noopener noreferrer" target="_blank" href="mailto:arifmohammedofficial@gmail.com"><i className='fab fa-google'></i></a>
+                    <a className="icon" rel="noopener noreferrer" target="_blank" href="https://www.instagram.com/arifmuhammed_0"><i className='fab fa-instagram'></i></a> 
                 </div>
                 <hr width='30%' style={{borderTop:'2px solid hsla(0,0%,100%,.1)'}}/>
                 <p className="footerText">© 2020 - Developed by Arif Mohammed</p>
