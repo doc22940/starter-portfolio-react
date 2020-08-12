836d083cc6fb51b09937930c477f3f745edec133
Arif Mohammed P.F <arifmohammedpf77@gmail.com>
2020-08-12 20:13:55 +0530

    style changed -navbar -project cards -footer icons

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -18,13 +18,13 @@ function Header(){
             <div className="container-fluid">
             <div className="navClass">
                     <Navbar bg="transparent" expand="md">
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Toggle className='navToggler' aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className="ml-auto" style={{minHeight: '3rem'}}>
-                                <Nav.Link onClick={()=>{scroll('#home')}} id="navhome" style={{color:'white'}}>Home</Nav.Link>
-                                <Nav.Link onClick={()=>{scroll('#about')}} id="navabout" style={{color:'white'}}>About</Nav.Link>
-                                <Nav.Link onClick={()=>{scroll('#projects')}} id="navprojects" style={{color:'white'}}>Projects</Nav.Link>
-                                <Nav.Link onClick={()=>{scroll('#contact')}} id="navcontact" style={{color:'white'}}>Contact</Nav.Link>                                
+                                
+                                <Nav.Link onClick={()=>{scroll('#about')}} id="navabout">About</Nav.Link>
+                                <Nav.Link onClick={()=>{scroll('#projects')}} id="navprojects">Projects</Nav.Link>
+                                <Nav.Link onClick={()=>{scroll('#contact')}} id="navcontact">Contact</Nav.Link>                                
                             </Nav>
                         </Navbar.Collapse>
                     </Navbar>
