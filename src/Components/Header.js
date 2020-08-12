f4ef04550a186119e5978afb41c0b14c770170fb
Arif Mohammed P.F <arifmohammedpf77@gmail.com>
2020-08-12 09:58:14 +0530

    Updates: - In package.json, github deploy scripts added. - NavToggle white color removed.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -18,7 +18,7 @@ function Header(){
             <div className="container-fluid">
             <div className="navClass">
                     <Navbar bg="transparent" expand="md">
-                        <Navbar.Toggle style={{backgroundColor:"whitesmoke"}} aria-controls="basic-navbar-nav" />
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className="ml-auto" style={{minHeight: '3rem'}}>
                                 <Nav.Link onClick={()=>{scroll('#home')}} id="navhome" style={{color:'white'}}>Home</Nav.Link>
